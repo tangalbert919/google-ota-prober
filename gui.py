c009a9daaef07783f013c904e9659412613dcb0e
Albert Tang <tangalbert919@yahoo.com>
2024-12-11 16:51:07 -0800

    Create basic GUI

---
+++ gui.py
@@ -0,0 +1,35 @@
+import flet as ft
+import probe
+
+def main(page: ft.Page):
+    prober = probe.Prober()
+    url = ""
+
+    def start_probe(e):
+        print("Button clicked")
+        print(fingerprint.value)
+        global url
+        url = prober.checkin(fingerprint.value, model.value)
+        if url is not None:
+            print("Download button enabled")
+            downloadBtn.disabled = False
+        page.update()
+    
+    def download(e):
+        print("Download button clicked")
+        global url
+        prober.download(url)
+
+    page.title = "Google OTA Prober"
+    fingerprint = ft.TextField(label="Enter fingerprint here")
+    model = ft.TextField(label="Enter model here")
+    downloadBtn = ft.ElevatedButton("Download", on_click=download, disabled=True)
+    page.add(ft.Column([
+        ft.Container(content=fingerprint, alignment=ft.alignment.center),
+        ft.Container(content=model, alignment=ft.alignment.center),
+        ft.Container(content=ft.ElevatedButton("Start probe", bgcolor="#FF8F8F", on_click=start_probe), alignment=ft.alignment.center),
+        ft.Container(content=downloadBtn, alignment=ft.alignment.center)
+    ], alignment=ft.MainAxisAlignment.CENTER))
+    page.update()
+
+ft.app(target=main)
