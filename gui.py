1099e47934fc93c0854aa5c75cc56bf5069b978e
Albert Tang <tangalbert919@yahoo.com>
2024-12-14 11:52:34 -0800

    Add update changelog dialog

--- gui.py
+++ gui.py
@@ -10,28 +10,37 @@ def main(page: ft.Page):
         if url is not None:
             update_info.value = "An update is available!"
             downloadBtn.disabled = False
+            update_dlg_btn.disabled = False
         else:
             update_info.value = "No update is available."
             downloadBtn.disabled = True
+            update_dlg_btn.disabled = True
         page.update()
     
     def download(e):
         print("Download button clicked")
         global url
         prober.download(url)
+    
+    update_dlg = ft.AlertDialog(
+        title=ft.Text("Update changelog"),
+        on_dismiss=lambda e: print("Dialog dismissed."),
+    )
 
     page.title = "Google OTA Prober"
     fingerprint = ft.TextField(label="Enter fingerprint here")
     model = ft.TextField(label="Enter model here")
     downloadBtn = ft.ElevatedButton("Download", on_click=download, disabled=True)
     update_info = ft.Text("Update info will be displayed here")
+    update_dlg_btn = ft.ElevatedButton("Changelog", on_click=lambda e: page.open(update_dlg), disabled=True)
 
     page.add(ft.Column([
         ft.Container(content=fingerprint, alignment=ft.alignment.center),
         ft.Container(content=model, alignment=ft.alignment.center),
         ft.Container(content=ft.ElevatedButton("Start probe", bgcolor="#FF8F8F", on_click=start_probe), alignment=ft.alignment.center),
         ft.Container(content=downloadBtn, alignment=ft.alignment.center),
-        ft.Container(content=update_info, alignment=ft.alignment.center)
+        ft.Container(content=update_info, alignment=ft.alignment.center),
+        ft.Container(content=update_dlg_btn, alignment=ft.alignment.center)
     ], alignment=ft.MainAxisAlignment.CENTER))
     page.update()
 
